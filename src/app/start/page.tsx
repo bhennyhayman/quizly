4b11589a2b3fc8d55f7283b7febadd33792d54c6
Bhenny Hayman <bengero686@gmail.com>
2025-10-12 14:28:05 +0000

    changes

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -55,7 +55,7 @@ export default function Start() {
       </div>
 
       <p className="font-bold bg-blue-100 rounded w-fit p-2 mx-5 mt-5 mb-2 min-md:mx-auto">Select Level</p>
-      <div className="grid grid-cols-3 mx-auto w-fit gap-5 max-sm:gap-2">
+      <div className="grid grid-cols-3 mx-auto w-fit gap-5 max-sm:flex">
         {difficulty.map((item,i) =>
         <button name={item} onClick={handleLevel}
         className={`${item === level ? "bg-green-600 text-white": ""} shadow-[1px_1px_5px_grey] rounded py-2 px-2`} key={i}>
